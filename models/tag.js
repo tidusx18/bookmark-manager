e97d3ff0b7706fade7e1b3595fd47f602ad35946
Daniel Victoriano <victoriano518@gmail.com>
2018-12-23 13:44:49 -0500

    Add: Remove all instances of deleted tag from bookmarks.

--- models/tag.js
+++ models/tag.js
@@ -1,8 +1,19 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+let ObjectId = mongoose.Types.ObjectId;
+let Bookmark = mongoose.model('Bookmark');
 
 let tagSchema = new Schema({
 	name: { type: String, required: true, max: 100 }
 });
 
-module.exports = mongoose.model('Tag', tagSchema);
+tagSchema.post('findOneAndDelete', (doc) => {
+	console.log('findOneAndDelete hook', doc)
+	Bookmark.updateMany(
+		{},
+		{ $pull: { tags: { _id: ObjectId(doc._id) } } },
+		(err, res) => { err ? console.log('Error - findOneAndDelete post hook: ', err) : null; }
+	)
+});
+
+module.exports = mongoose.model('Tag', tagSchema); 
