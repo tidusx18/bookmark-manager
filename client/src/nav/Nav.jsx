bc7be12459b1b1b0d69db9011a46ee5a057708c1
tidusx18 <victoriano518@gmail.com>
2019-01-09 16:16:15 -0500

    Update (Nav/AppBar): Left anchored Drawer nav. Fixed AppBar with title.

--- client/src/nav/Nav.jsx
+++ client/src/nav/Nav.jsx
@@ -1,48 +1,159 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import LabelIcon from '@material-ui/icons/Label';
+import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import  Tags from '../tag/TagList.jsx';
 import  Bookmarks from '../bookmark/Bookmarks.jsx';
 
+const drawerWidth = 240;
+
 const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
-    color: '#ffffff'
+  root: {
+    display: 'flex',
   },
-  grow: {
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing.unit * 3,
   },
 });
 
 class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
 
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button component='a' href='/'>
+            <ListItemIcon>
+              <BookmarksIcon />
+            </ListItemIcon>
+            <ListItemText primary='Bookmarks' />
+          </ListItem>
+          <ListItem button component='a' href='/tags'>
+            <ListItemIcon>
+              <LabelIcon />
+            </ListItemIcon>
+            <ListItemText primary='Tags' />
+          </ListItem>
+        </List>
+        <Divider />
+      </div>
+    );
 
     return (
-      <Router>
-        <div>
-        <AppBar position='relative'>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position='fixed' className={classes.appBar}>
           <Toolbar>
-            <Button className={classes.button} color='default' href="/">Bookmarks</Button>
-            <Button className={classes.button} color='default' href="/tags">Tags</Button>
-            <div className={classes.grow} />
+            <IconButton
+              color='inherit'
+              aria-label='Open drawer'
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='title' color='inherit' noWrap>
+              {document.location.pathname === '/' ? 'Bookmarks' : 'Tags'}
+            </Typography>
           </Toolbar>
         </AppBar>
-          <hr />
-
-          <Route exact path="/" component={Bookmarks} />
-          <Route path="/tags" component={Tags} />
-        </div>
-      </Router>
-    )
+        <nav className={classes.drawer}>
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation='css'>
+            <Drawer
+              container={this.props.container}
+              variant='temporary'
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation='css'>
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant='permanent'
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Route exact path='/' component={Bookmarks} />
+          <Route path='/tags' component={Tags} />
+        </main>
+      </div>
+    );
   }
 }
 
-export default withStyles(styles)(Nav);
+// Nav.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   // Injected by the documentation to work in an iframe.
+//   // You won't need it on your project.
+//   container: PropTypes.object,
+//   theme: PropTypes.object.isRequired,
+// };
+
+export default withStyles(styles, { withTheme: true })(Nav);
